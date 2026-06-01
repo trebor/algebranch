0d1a82738fae1ca8b33d301b6708c426ce8a9495
Robert Harris <github.com@trebor.org>
2026-06-01 12:50:45 -0700

    chore(ui): suppress third-party extension-induced hydration warnings on html and body tags

--- ui/src/app/layout.tsx
+++ ui/src/app/layout.tsx
@@ -26,8 +26,9 @@ export default function RootLayout({
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      suppressHydrationWarning
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col" suppressHydrationWarning>{children}</body>
     </html>
   );
 }
